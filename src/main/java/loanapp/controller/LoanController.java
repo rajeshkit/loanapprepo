26491ccae35dc7523bc75ebbc5eb2bb4ed659e90
rajesh n <rajesh.kit@gmail.com>
2025-01-24 14:21:35 +0530

    new feature1 is added

--- src/main/java/loanapp/controller/LoanController.java
+++ src/main/java/loanapp/controller/LoanController.java
@@ -4,4 +4,7 @@ public class LoanController {
 	public String welcome() {
 		return "welcome to rest api";
 	}
+	public void newFeature1() {
+		System.out.println("new feature is implemented");
+	}
 }	
